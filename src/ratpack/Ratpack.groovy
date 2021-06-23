76a09c51855e193f0e66ea848b1e0eccc68434a9
nicolas-f <[omitted]>
2021-06-23 16:49:51 +0200

    no h2 deps for version issues

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,9 +1,6 @@
 import groovy.sql.Sql
-import org.flywaydb.core.Flyway
-import ratpack.h2.H2Module
 import ratpack.service.Service
 import ratpack.service.StartEvent
-
 import javax.sql.DataSource
 
 import static ratpack.groovy.Groovy.ratpack
@@ -11,24 +8,10 @@ import static ratpack.jackson.Jackson.json
 
 ratpack {
 
-    bindings {
-
-        // Use H2 in-memory database
-        module(new H2Module("sa", "", "jdbc:h2:mem:convertions;DB_CLOSE_DELAY=-1"))
-
-        // Migrate flyway scripts on startup
-        bindInstance new Service() {
-            void onStart(StartEvent event) {
-                new Flyway(dataSource: event.registry.get(DataSource)).migrate()
-            }
-        }
-
-
-    }
-
     handlers {
 
         get {
+
             render json([
                     "result" : "hello"
             ])
