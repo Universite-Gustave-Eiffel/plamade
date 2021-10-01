a43f407f77404bc95a124a912b09f4e97ae473e3
nicolas-f <[omitted]>
2021-10-01 16:08:24 +0200

    move platform to subdir

---
+++ computation_core/src/main/java/Main.java
@@ -0,0 +1,49 @@
+import org.h2.util.OsgiDataSourceFactory;
+import org.h2gis.functions.factory.H2GISFunctions;
+import org.osgi.service.jdbc.DataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * NoiseModelling is an open-source tool designed to produce environmental noise maps on very large urban areas. It can be used as a Java library or be controlled through a user friendly web interface.
+ *
+ * This version is developed by the DECIDE team from the Lab-STICC (CNRS) and by the Mixt Research Unit in Environmental Acoustics (Université Gustave Eiffel).
+ * <http://noise-planet.org/noisemodelling.html>
+ *
+ * NoiseModelling is distributed under GPL 3 license. You can read a copy of this License in the file LICENCE provided with this software.
+ *
+ * Contact: contact@noise-planet.org
+ *
+ */
+
+public class Main {
+
+    public static DataSource createDataSource(String user, String password, String dbDirectory, String dbName, boolean debug) throws SQLException {
+        // Create H2 memory DataSource
+        org.h2.Driver driver = org.h2.Driver.load();
+        OsgiDataSourceFactory dataSourceFactory = new OsgiDataSourceFactory(driver);
+        Properties properties = new Properties();
+        String databasePath = "jdbc:h2:" + new File(dbDirectory, dbName).getAbsolutePath();
+        properties.setProperty(DataSourceFactory.JDBC_URL, databasePath);
+        properties.setProperty(DataSourceFactory.JDBC_USER, user);
+        properties.setProperty(DataSourceFactory.JDBC_PASSWORD, password);
+        if(debug) {
+            properties.setProperty("TRACE_LEVEL_FILE", "3"); // enable debug
+        }
+        DataSource dataSource = dataSourceFactory.createDataSource(properties);
+        // Init spatial ext
+        try (Connection connection = dataSource.getConnection()) {
+            H2GISFunctions.load(connection);
+        }
+        return dataSource;
+
+    }
+
+    public static void main(String... args) throws Exception {
+
+    }
+}
