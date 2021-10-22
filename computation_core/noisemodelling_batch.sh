78de7b64ad46252d9c6407c42dae038625f44c2e
nicolas-f <[omitted]>
2021-10-22 14:52:12 +0200

    run without using gradle

--- computation_core/noisemodelling_batch.sh
+++ computation_core/noisemodelling_batch.sh
@@ -4,13 +4,7 @@
 # run with this command
 # sbatch noisemodelling_batch.sh
 dbpath="/home2020/home/cerema/nfortin/dep38/"
-
-# copy gradle cache (faster build time)
-echo "Sync gradle cache"
-rsync -a --stats ~/.gradle/ /scratch/job."$SLURM_JOB_ID"/.gradle/
-export GRADLE_USER_HOME=/scratch/job."$SLURM_JOB_ID"/.gradle/
-
-echo "gradle path is now $GRADLE_USER_HOME"
+codepath="/home2020/home/cerema/nfortin/plamade/plamade/computation_core"
 
 echo "copy data to local node"
 mkdir /scratch/job."$SLURM_JOB_ID"/data/
@@ -18,13 +12,15 @@ rsync -a "$dbpath" /scratch/job."$SLURM_JOB_ID"/data/
 
 echo "copy code to local node"
 mkdir /scratch/job."$SLURM_JOB_ID"/code/
-rsync -a ../ /scratch/job."$SLURM_JOB_ID"/code
+rsync -a "$codepath"/build /scratch/job."$SLURM_JOB_ID"/code/
 
 echo "run noisemodelling"
-cd /scratch/job."$SLURM_JOB_ID"/code && ./gradlew computation_core:run --args="-n$SLURM_ARRAY_TASK_ID -w/scratch/job.$SLURM_JOB_ID/data/"
+cd /scratch/job."$SLURM_JOB_ID"/code && java -jar build/libs/computation_core.jar -n"$SLURM_ARRAY_TASK_ID" -w/scratch/job."$SLURM_JOB_ID"/data/
+
 echo "copy results"
 mkdir -p ~/job_"$SLURM_ARRAY_JOB_ID"
 cp /scratch/job."$SLURM_JOB_ID"/data/*.shp ~/job_"$SLURM_ARRAY_JOB_ID"/
 cp /scratch/job."$SLURM_JOB_ID"/data/*.dbf ~/job_"$SLURM_ARRAY_JOB_ID"/
 cp /scratch/job."$SLURM_JOB_ID"/data/*.shx ~/job_"$SLURM_ARRAY_JOB_ID"/
 cp /scratch/job."$SLURM_JOB_ID"/data/*.prj ~/job_"$SLURM_ARRAY_JOB_ID"/
+cp /scratch/job."$SLURM_JOB_ID"/data/*.csv ~/job_"$SLURM_ARRAY_JOB_ID"/
