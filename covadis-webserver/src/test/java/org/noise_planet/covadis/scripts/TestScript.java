8eb13c0e3aa1b245ee0eb22f3f836077299e649c
nicolas-f <github@nettrader.fr>
2026-06-08 15:40:42 +0200

    update java version

--- covadis-webserver/src/test/java/org/noise_planet/covadis/scripts/TestScript.java
+++ covadis-webserver/src/test/java/org/noise_planet/covadis/scripts/TestScript.java
@@ -3,10 +3,15 @@ package org.noise_planet.covadis.scripts;
 
 import org.junit.jupiter.api.Test;
 import org.noise_planet.covadis.scripts.Slurm.Main_Remote_Script;
+import org.noise_planet.covadis.webserver.NoiseModellingHPCServerHttpTest;
+import org.noise_planet.noisemodelling.scripts.Database_Manager.Table_Visualization_Data;
+import org.noise_planet.noisemodelling.scripts.Import_and_Export.Import_File;
 
 import java.sql.SQLException;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestScript extends JDBCTestCase {
     @Test
@@ -25,5 +30,14 @@ public class TestScript extends JDBCTestCase {
         var rs = connection.createStatement().executeQuery("SELECT COUNT(*) FROM receivers");
         rs.next();
         assertEquals((int)(Math.ceil(5000 / 32.0)), rs.getInt(1));
+    }
+
+    @Test
+    public void testDisplayTableData() throws SQLException {
+        new Import_File().exec(connection, Map.of("pathFile", NoiseModellingHPCServerHttpTest.class.getResource("receivers.shp").getFile()));
+        String res = new Table_Visualization_Data().exec(connection, Map.of("tableName", "receivers")).toString();
+        assertTrue(res.contains("The total number of rows is 830"));
+        assertTrue(res.contains("The srid of the table is 2154"));
+        assertTrue(res.contains("POINT Z(223495.9880411485 6757167.98900822 0)"));
     }
 }
